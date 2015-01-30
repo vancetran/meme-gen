4d1b926f0b659af683066e292cd7df3df00fc7ca
Vance Tran <vancetran@gmail.com>
2015-01-29 23:17:59 -0800

    Basic file upload

--- app/js/app.js
+++ app/js/app.js
@@ -12,9 +12,25 @@ MemeGen = function() {
     });
   }
 
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        $('#blah')
+        .attr('src', e.target.result)
+        .width(150)
+        .height(200);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
   // Public Interface
   return {
-    init: init
+    init: init,
+    readURL: readURL
   };
 
 }();
