bead048ac6876eebc36c91f7c7074e01776619c6
Vance Tran <v@vancetran.com>
2015-03-03 10:30:39 -0800

    Brand Colors

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,14 @@ var MemeGen = MemeGen || {};
 
 MemeGen = function() {
 
+  var swatches = {
+    grayscaleSimple: ["black", "#333", "#666", "#ccc", "white"],
+    ucBrandColors: ["#1295D8","#FFB511","#005581","#72CDF4", "#FFD200", "#FFE552", "#FF6E1B",
+      "#FF8F28", "#E44C9A", "#FEB2E0", "#00778B", "#00A3AD", "#7C7E7F", "#8F8884", "#BEB6AF", "#DBD5CD", "#B4975A"]
+  };
+
+  var swatchCombined = swatches.grayscaleSimple.concat(swatches.ucBrandColors);
+
   function init(){
     console.log("Team boo.");
 
@@ -49,8 +57,7 @@ MemeGen = function() {
       placement: 'right',
       hsvpanel: true,
       previewformat: 'hex',
-
-
+      swatches: swatchCombined,
 
       onchange: function(container, color) {
 
